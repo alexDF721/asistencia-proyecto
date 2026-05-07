28af186b83d83c7f4c51b2a7f2e683e309765a07
unknown <fumanerialex2@gmail.com>
2026-05-06 21:23:12 -0300

    Fix puerto Railway

--- server.js
+++ server.js
@@ -43,6 +43,8 @@ setInterval(() => {
     }
 }, 60000);
 
-app.listen(3000, () => {
-    console.log("Servidor corriendo en http://localhost:3000");
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+    console.log(`Servidor corriendo en puerto ${PORT}`);
 });
